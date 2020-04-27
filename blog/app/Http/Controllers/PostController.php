2937d13dd7043d8092a3bedbb972533e747a027e
elesos <admin@elesos.com>
2020-04-27 13:08:54 +0800

    uodate by robot

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -6,18 +6,24 @@ use Illuminate\Http\Request;
 
 
 use App\Post;
-
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public  function article_auth(Request $request){
 		//查询并返回一条记录
-        $article = Post::where('state', 0)
+       /*  $article = Post::where('state', 0)
 			     ->orderBy('id', 'asc')->select('id', 'title', 'content', 'state', 'author')
 			     ->first();
-		dd($article);
-	    return;
+		dd($article); //$article->id
+	    return; */
 		
+		//更新数据库
+		$affected = DB::table('article_house')
+           ->where('id', 2)
+           ->update(['state' => 1, 'author' => 'test']);
+		dd($affected);
+		return;
 	
     }
 }
