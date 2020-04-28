17dd367b71e52701ae7cf2b4dd2e9b7537cfe252
elesos <admin@elesos.com>
2020-04-28 16:13:16 +0800

    uodate by elesos

--- blog/resources/js/components/Home.vue
+++ blog/resources/js/components/Home.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
-        <el-steps :active="1" finish-status="success" simple style="margin-top: 20px">
-		  <el-step title="步骤 1" ></el-step>
-		  <el-step title="步骤 2" ></el-step>
-		  <el-step title="步骤 3" ></el-step>
+        <el-steps finish-status="success" simple style="margin-top: 20px">
+		 
+		  
+		    <el-step v-for="(value, name, index) in modules" v-bind:title="name">
+			
+		</el-step>
+		
+		
 		</el-steps>
 
     </div>
